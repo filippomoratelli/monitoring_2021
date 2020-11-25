01ede50ffdaf20dffc0209cf823725f39a4f8da9
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2020-11-25 13:03:00 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -30,3 +30,25 @@ plot(cadmium, lead)
 
 # scatterplot matrices (all possible graphs)
 pairs(meuse)
+
+
+# lecture 2
+
+# recall the sp package and meuse dataset
+library(sp)
+data(meuse)
+
+# pairs with soil variables (Cd, Cu, Pb, Zn = 3, 4, 5, 6 columns)
+# from (,) column 3 to (:) column 6, using []
+pairs(meuse[,3:6])
+
+# use the names of the columns instead (if we use a different system)
+# tilde = option + 5 ~
+pairs(~ cadmium + copper + lead + zinc, data=meuse)
+
+# change color (using "x") COL
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red")
+# change symbol to filled triangles (no "x") PCH
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17)
+# increase size of triangles CEX
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17, cex=3)
