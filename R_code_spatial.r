3113a9e164d91bfca0dc6adfc28661a8fb2426c8
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2020-11-25 18:27:22 +0100

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -22,3 +22,46 @@ bubble(meuse, "zinc")
 
 bubble(meuse, "lead")
 bubble(meuse, "lead", col="red")
+
+
+# ggplot 2 
+install.packages("ggplot2")
+library(ggplot2)
+
+# ecological dataframe
+# biofuels
+# array of values: c
+biofuels <- c(120, 200, 350, 570, 750)
+biofuels
+# oxydative enzimes
+oxydative <- c(1200, 1300, 21000, 34000, 50000)
+oxydative
+
+# create a dataframe (table)
+d <- data.frame(biofuels, oxydative)
+d
+# or just
+data.frame(biofuels, oxydative)
+
+# plot using ggplot
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point()
+# aes stands for aestethics = data (x,y)
+# geom_point is to create a graphic visualization of the values
+# change size and colors of the points
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point(size = 5, col = "red")
+
+# line instead of points
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_line()
+
+# both points and a line
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point(size = 5, col = "red")  +  geom_line() 
+
+# polygons
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_polygon()
+
+
+#### import data from an external source
+
+# it should be something like setwd("/Desktop/UniBo/Monitoring/lab/")
+# instead do command + D and choose folder
+covid <- read.table("covid_agg.csv", header=TRUE)
