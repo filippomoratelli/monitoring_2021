3a0075c67d86656952db035e2c307ac82436f96f
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2020-12-03 13:08:26 +0100

    Update R_code_point_pattern.r

--- R_code_point_pattern.r
+++ R_code_point_pattern.r
@@ -3,8 +3,13 @@
 install.packages("spatstat")
 library(spatstat)
 
+# lab folder
+# cmd + D then select lab folder
+
+# add file from lab folder
 covid <- read.table("covid_agg.csv", header=TRUE)
 covid
+head(covid)
 
 # density of the covid data
 # planar point pattern in spatstat
@@ -13,3 +18,9 @@ attach(covid)
 covid_planar <- ppp(lon, lat, c(-180,180), c(-90,90))
 density_map <- density(covid_planar)
 plot(density_map)
+
+points(covid_planar)
+
+cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar)
