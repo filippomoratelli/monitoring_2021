274ddc8ccd712f3958a2a3511f513c3fe5037f1f
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2020-12-03 16:23:12 +0100

    Update R_code_point_pattern.r

--- R_code_point_pattern.r
+++ R_code_point_pattern.r
@@ -18,9 +18,34 @@ attach(covid)
 covid_planar <- ppp(lon, lat, c(-180,180), c(-90,90))
 density_map <- density(covid_planar)
 plot(density_map)
-
 points(covid_planar)
 
-cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
+# change color palette
+cl <- colorRampPalette(c('yellow','orange','red'))(100) 
 plot(density_map, col = cl)
 points(covid_planar)
+
+# more colors
+cl <- colorRampPalette(c('blue','yellow','orange','red','magenta'))(100) 
+plot(density_map, col = cl)
+points(covid_planar)
+
+# putting countries on top of the map using another package from OSGeo
+install.packages("rgdal")
+library(rgadl)
+
+# download coastlines
+# download the files (three types) and put them in the work directory
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+# add coastlines to the map
+cl <- colorRampPalette(c('blue','yellow','orange','red','magenta'))(100)
+plot(density_map, col = cl)
+points(covid_planar)
+plot(coastlines, add = TRUE)
+
+# change points (add PCH to points)
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100)
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
