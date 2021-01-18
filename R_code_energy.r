1011c4e06c4bcab79646f76aafd75b42e2275658
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2021-01-18 11:00:50 +0100

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -33,3 +33,60 @@ plot(copNDVI, col=clymax)
 ext <- c(0,20,35,55)
 copNDVI_Italy <- crop(copNDVI, ext)
 plot(copNDVI_Italy, col=clymax)
+
+###########
+
+library(raster)
+library(RStoolbox)
+
+# change working directory (cmd D) and then name file
+defor1 <- brick("defor1_.jpg")
+
+# to see data (under "names" you see names of layers like defor1_.1, .2, ...)
+defor1
+
+# plot using RGB
+plotRGB(defor1, 1, 2, 3, stretch="Lin")
+plotRGB(defor1, 2, 1, 3, stretch="Lin")
+
+# same things for the other picture
+defor2 <- brick("defor2_.jpg")
+defor2
+plotRGB(defor2, 2, 1, 3, stretch="Lin")
+
+# plot both
+par(mfrow=c(2,1))
+plotRGB(defor1, 2, 1, 3, stretch="Lin")
+plotRGB(defor2, 2, 1, 3, stretch="Lin")
+
+dev.off()
+
+# DVI using layers (image name $ layer name, we do NIR layer - red layer)
+dvi1 <- defor1$defor1_.1 - defor1$defor1_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+plot(dvi1, col=cl)
+
+dvi2 <- defor2$defor2_.1 - defor2$defor2_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+plot(dvi2, col=cl)
+
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+
+# biomass difference before and after cut
+difdvi <- dvi1 - dvi2
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cldif, main="amount of energy lost")
+
+# histogram
+hist(difdvi, col="red")
+
+# final par with all plots
+par(mfrow=c(3,2))
+plotRGB(defor1, 2, 1, 3, stretch="Lin")
+plotRGB(defor2, 2, 1, 3, stretch="Lin")
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+plot(difdvi, col=cldif, main="amount of energy lost")
+hist(difdvi, col="red")
